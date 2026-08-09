fa72f2d27fe57488f1449531be7fcaf8ee5633ec
Jonathan Hallström <lmj.hallstrom@gmail.com>
2026-08-09 16:01:34 +0200

    nn
    soon to be ue

--- src/eval.rs
+++ src/eval.rs
@@ -21,151 +21,62 @@
  * SOFTWARE.
  */
 
-use std::array;
-
-use crate::bitboard::Bitboard;
 use crate::board::Position;
-use crate::core::{Direction, Piece, PieceType, Player, Square};
-use crate::search::Score;
+use crate::core::{PieceType, Player, Square};
+use crate::search::{SCORE_WIN, Score};
 
-#[static_init::dynamic]
-static RINGS: [Bitboard; 5] = {
-    let mut covered = Bitboard::from_raw(1 << 14 | 1 << 15 | 1 << 20 | 1 << 21);
-    let mut curr = covered;
-    array::from_fn(|_| {
-        let r = curr;
-        curr = (curr << 6 | curr >> 6 | curr << 1 | curr >> 1) & !covered;
-        covered |= curr;
-        r
-    })
-};
+const HL: usize = 32;
+const QA: i32 = 255;
+const QB: i32 = 64;
+const SCALE: i32 = 400;
 
-const ADJACENT_MASKS: [Bitboard; Square::COUNT] = {
-    let mut masks = [Bitboard::empty(); Square::COUNT];
+#[repr(C, align(64))]
+struct Network {
+    l0w: [[i16; HL]; 216],
+    l0b: [i16; HL],
+    l1w: [[i16; HL]; 2],
+    l1b: i16,
+}
 
-    let mut sq_idx = 0;
-    while let Some(sq) = Square::from_raw(sq_idx) {
-        let bb = sq.bb();
-        let bb = bb
-            .shift(Direction::Up)
-            .or(bb.shift(Direction::Down))
-            .or(bb.shift(Direction::Left))
-            .or(bb.shift(Direction::Right));
-        masks[sq.idx()] = bb;
-        sq_idx += 1;
-    }
+static NET: Network = unsafe { std::mem::transmute(*include_bytes!(env!("EVALFILE"))) };
 
-    masks
-};
-
-#[rustfmt::skip]
-const CAP_PSQT: [Score; Square::COUNT] = [
-    -20,  -5,  -5,  -5,  -5, -20,
-     -5,  10,  18,  18,  10,  -5,
-     -5,  18,  35,  35,  18,  -5,
-     -5,  18,  35,  35,  18,  -5,
-     -5,  10,  18,  18,  10,  -5,
-    -20,  -5,  -5,  -5,  -5, -20,
-];
-
-#[must_use]
-fn static_eval_player(pos: &Position, player: Player, komi: u32) -> Score {
-    let flat_bb = pos.player_piece_bb(PieceType::Flat.with_player(player));
-    let flats = (flat_bb.popcount() + komi) as Score;
-    let flats = flats * 75;
-
-    let flats_in_hand = pos.flats_in_hand(player) as Score;
-    let flats_in_hand = flats_in_hand * -13;
-
-    let road_bb = pos.roads(player);
-
-    let adj_horz = road_bb & road_bb.shift(Direction::Left);
-    let adj_vert = road_bb & road_bb.shift(Direction::Down);
-
-    let line_horz = adj_horz & adj_horz.shift(Direction::Left);
-    let line_vert = adj_vert & adj_vert.shift(Direction::Down);
-
-    let adj_value = (adj_horz.popcount() + adj_vert.popcount()) as Score;
-    let line_value = (line_horz.popcount() + line_vert.popcount()) as Score;
-
-    let adj_value = adj_value * 9;
-    let line_value = line_value * 7;
-
-    let stacks = &pos.stacks();
-    let player_flip = if player == Player::P2 { u64::MAX } else { 0 };
-
-    let mut support_score = 0;
-    let mut captive_score = 0;
-
-    for sq in pos.player_bb(player) {
-        let mut height = stacks.height(sq);
-
-        if height == 1 {
-            continue;
-        }
-
-        let mut players = stacks.players(sq) ^ player_flip;
-
-        if height > 7 {
-            players >>= height - 7;
-            height = 7;
-        }
-
-        let mask = (1 << (height - 1)) - 1;
-
-        let support_count = (!players & mask).count_ones() as Score;
-        let captive_count = (players & mask).count_ones() as Score;
-
-        match stacks.top(sq).unwrap() {
-            PieceType::Flat => {
-                support_score += support_count * 30;
-                captive_score += captive_count * -40;
-            }
-            PieceType::Wall => {
-                support_score += support_count * 35;
-                captive_score += captive_count * -15;
-            }
-            PieceType::Capstone => {
-                support_score += support_count * 40;
-                captive_score += captive_count * -20;
-            }
-        }
-    }
-
-    let isolated_mask = pos.occ() & !flat_bb;
-
-    let mut psqt_score = 0;
-    let mut isolated_cap_score = 0;
-
-    for cap_sq in pos.player_piece_bb(PieceType::Capstone.with_player(player)) {
-        psqt_score += CAP_PSQT[cap_sq.idx()];
-
-        let adjacent = ADJACENT_MASKS[cap_sq.idx()];
-        if (adjacent & isolated_mask).is_empty() {
-            isolated_cap_score -= 50;
-        }
-    }
-
-    flats + flats_in_hand + adj_value + line_value + support_score + captive_score + psqt_score + isolated_cap_score
+fn feature(perspective: Player, side: Player, pt: PieceType, sq: Square) -> usize {
+    // TODO: was tired and got side and piecetype backwards
+    pt.idx() * 72 + usize::from(side != perspective) * 36 + sq.idx()
 }
 
 #[must_use]
 pub fn static_eval(pos: &Position) -> Score {
-    let p1_score = static_eval_player(pos, Player::P1, 0);
-    let p2_score = static_eval_player(pos, Player::P2, Position::KOMI);
+    let stm = pos.stm();
+    let stacks = pos.stacks();
 
-    let p1_flat_bb = pos.player_piece_bb(Piece::P1Flat);
-    let p2_flat_bb = pos.player_piece_bb(Piece::P2Flat);
+    let mut accs = [NET.l0b; 2];
 
-    let flat_position_quality_diff = RINGS
-        .iter()
-        .zip([2, 8, -5, -15, -40])
-        .map(|(&ring, value)| {
-            (p1_flat_bb & ring).popcount() as i32 * value - (p2_flat_bb & ring).popcount() as i32 * value
-        })
-        .sum::<i32>();
+    for side in [Player::P1, Player::P2] {
+        for sq in pos.player_bb(side) {
+            let pt = stacks.top(sq).unwrap();
 
-    let eval = p1_score - p2_score + flat_position_quality_diff;
+            for perspective in [Player::P1, Player::P2] {
+                for (a, w) in accs[perspective.idx()]
+                    .iter_mut()
+                    .zip(&NET.l0w[feature(perspective, side, pt, sq)])
+                {
+                    *a += w;
+                }
+            }
+        }
+    }
 
-    eval * pos.stm().sign() + 30
+    let mut sum = 0;
+
+    for (perspective, weights) in [stm, stm.flip()].iter().zip(&NET.l1w) {
+        for (&a, &w) in accs[perspective.idx()].iter().zip(weights) {
+            let c = a.clamp(0, QA as i16);
+            sum += i32::from(c) * i32::from(c * w);
+        }
+    }
+
+    let eval = (sum / QA + i32::from(NET.l1b)) * SCALE / (QA * QB);
+
+    (eval as Score).clamp(-SCORE_WIN, SCORE_WIN)
 }
